418b64b5668a22e52aa5a3b080a168ee7e10d38f
Dave Farrer <dave.farrer89@gmail.com>
2018-06-02 15:16:19 -0400

    add loading animation

--- src/components/Daily.js
+++ src/components/Daily.js
@@ -4,13 +4,12 @@ import defaultImage from "../images/default-nasa.png";
 class Daily extends React.Component {
 	constructor(props) {
 		super(props);
-		let today = new Date();
 		this.state = {
-			title: "NASA Image of the Day",
-			description: "Description for Image of the Day",
-			url: defaultImage,
-			date: today.toLocaleDateString("en-US")
+			url: "",
+			dataLoaded: false,
+			imageLoaded: false
 		}
+		this.onImageLoad = this.onImageLoad.bind(this);
 	}
 	componentDidMount() {
 		fetch("https://api.nasa.gov/planetary/apod?api_key=gLolLfXqaaTb7xnWrJrTiCABn0IOEElx0PrmA8bc")
@@ -21,19 +20,40 @@ class Daily extends React.Component {
 				title: json.title,
 				description: json.explanation,
 				url: json.url,
-				date: json.date
+				date: json.date,
+				dataLoaded: true
 			});
 		});
 	}
 
+	onImageLoad() {
+		this.setState({
+			imageLoaded: true
+		});
+	}
+
 	render() {
+		const dataLoaded = this.state.dataLoaded;
 		return (
-			<figure>
-				<h1 className="daily__title">{this.state.title}</h1>
-				<h4 className="daily__date">{this.state.date}</h4>
-				<a href={this.state.url}><img className="daily__image" src={this.state.url} alt="NASA Image of the Day" /></a>
-				<h3 className="daily__description">{this.state.description}</h3>
-			</figure>
+			<div>
+				{dataLoaded ? (
+					<figure>
+						<h1 className="daily__title">{this.state.title}</h1>
+						<h4 className="daily__date">{this.state.date}</h4>
+						<div className="daily__imageContainer">
+							{this.state.imageLoaded ? (
+								<img className="daily__image" src={this.state.url} alt={this.state.title} />
+							) : (
+								<div className="loader"></div>
+							)}
+						</div>
+						<h3 className="daily__description">{this.state.description}</h3>
+					</figure>
+				) : (
+					<div className="loader"></div>
+				)}
+					<img className="hidden" onLoad={this.onImageLoad} src={this.state.url} />
+			</div>
 		);
 	}
 }

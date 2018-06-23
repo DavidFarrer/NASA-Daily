72ed43096464ddf3ea96e87052ca19e33d0f672a
Dave Farrer <dave.farrer89@gmail.com>
2018-06-22 23:45:56 -0400

    add pass distance to near earth objects

--- src/components/NearEarth.js
+++ src/components/NearEarth.js
@@ -39,7 +39,7 @@ class NearEarth extends React.Component {
 						return (
 							<li key={nearEarthObject.id} className="nearEarth__object">
 								{nearEarthObject.name}
-								{nearEarthObject.missAstronomicalUnits}
+								<p className="nearEarth__distance">Distance of Pass: {(+nearEarthObject.missAstronomicalUnits).toFixed(2)}AU</p>
 							</li>
 						);
 					})
